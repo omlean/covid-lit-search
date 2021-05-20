07b38e8652c3d625b1cf26bdac146c61923b0df0
Oliver Lean <omlean@gmail.com>
2021-05-20 12:05:57 -0600

    New LDA model results

--- my_lda.py
+++ my_lda.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 import re
+import json
 
 import matplotlib.pyplot as plt
 
@@ -121,9 +122,93 @@ def topics_per_doc(model, corpus):
 ######################################################################################################
 
 # list top words in topic
-def topic_words(topic_no, topn=30):
+def topic_words(topic_no, model, corpus, topn=20):
     ids = model.get_topic_terms(topic_no, topn=50)
     words = [corpus.id2word[i] for i, amt in ids]
     return words
+
+######################################################################################################
+######################################################################################################
+
+# Investigation functions
+
+def print_pred(query, corpus, model):
+    """Prints details about input query relative to a corpus and model:
+    - The cleaned query text
+    - The bag of words
+    - Query topic predictions
+    - Topics for individual words from the query."""
+    
+    def word_topics(word_id):
+        return(model.get_term_topics(word_id, minimum_probability=None))
+
+    print(query, '\n')
+    query = clean_text_lda(query)
+    print(query, '\n')
+    q_vec = corpus.dictionary.doc2bow(query.split())
+    print("bow: ")
+    for i, n in q_vec:
+        print(corpus.id2word[i], n)
+    pred = model[q_vec]
+    pred.sort(key=lambda x: x[1])
+    pred = pred[::-1]
+    print("\nTopic predictions: ")
+    print(pred, '\n')
+    for word, _ in q_vec:
+        print(corpus.id2word[word], word_topics(word))
+        
+######################################################################################################
+
+def uid_pred(uid, model, corpus, metadata):
+    """Retrieves predictions for input UID"""
+    print(metadata[metadata.cord_uid == uid].title.iloc[0])
+    path = [path for path in corpus.doc_path_list if uid in path][0]
+    bow = corpus.get_doc_bow(path)
+    pred = model[bow]
+    pred.sort(key=lambda x: x[1])
+    pred = pred[::-1]
+    print("\nTopic predictions: ")
+    print(pred)
+    
+######################################################################################################
+
+def search_titles(query, model, corpus, dictionary, metadata, print_titles=True):
+    """Run search on the query, returns DataFrame of results. Optionally prints titles."""
+    results_df = my_lda.lda_search(query, model, corpus, dictionary, metadata)
+    if print_titles:
+        for title in results_df.title.values:
+            print(title, '\n')
+    return results_df
+
+######################################################################################################
+
+def get_body_text_from_path(path):
+    """Input:
+    path [string]: full path of target file
+    Output:
+    String of body text"""
+    blocks = []
+    with open(path, 'r') as file:
+        j = json.load(file)
+        for block in j['body_text']:
+            blocks.append(block['text'])
+    fulltext = '\n\n'.join(blocks)
+    return fulltext
+
+######################################################################################################
+
+def print_parsed_text(uid, version='raw', clean_directory='data/cord-19/body_text/lda_raw/'):
+    """In: uid
+    Out: Raw or Cleaned text from """
+    if version == 'raw':
+        directory = 'data/cord-19/body_text/lda_raw/'
+        suffix = '.txt'
+    if version == 'clean':
+        directory = clean_directory
+        suffix = '_clean.txt'
+    path = directory + uid + suffix
+    with open(path, 'r') as file:
+        text = file.read()
+    print(text)
 
 ######################################################################################################
